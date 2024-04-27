98c9e2b91e183cca87194d9624e127a593bc4455
Roman Potapov <rummaan@me.com>
2024-04-27 18:58:10 -0400

    v0003

---
+++ BB Quotes/CharacterView.swift
@@ -0,0 +1,18 @@
+//
+//  CharacterView.swift
+//  BB Quotes
+//
+//  Created by Roman Potapov on 4/26/24.
+//
+
+import SwiftUI
+
+struct CharacterView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CharacterView()
+}
